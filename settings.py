5c166b0afe7e671aba38b136fd21fef582c20b9c
NoelleRensing <79868313+NoelleRensing@users.noreply.github.com>
2021-04-29 20:11:02 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ SESSION_CONFIGS = [
 # e.g. self.session.config['participation_fee']
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=0.01, participation_fee=0.00, doc=""
+    real_world_currency_per_point=0.01, participation_fee=2.00, doc=""
 )
 
 # ISO-639 code
